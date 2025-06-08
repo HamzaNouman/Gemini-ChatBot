98c1ba4867f6ea382decb2d925da82cbd27b2bd1
HamzaNouman <137004030+HamzaNouman@users.noreply.github.com>
2025-06-08 08:02:13 +0300

    Add files via upload

---
+++ gemini.py
@@ -0,0 +1,24 @@
+from google import genai
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
+
+
+
+while True:
+    meassage = input("User: ")
+
+    if meassage.lower() == "exit":
+            print("Bot: Goodbye! 👋")
+            break
+
+    response = client.models.generate_content(
+    model="gemini-2.5-flash-preview-05-20", contents=meassage
+     )
+    
+
+    print(response.text)
+
